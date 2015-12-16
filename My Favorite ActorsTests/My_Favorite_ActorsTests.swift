ff7cfedac0a49953dd2c3ae7fe3c7fa39517f125
Abdelrahman Mohamed <obada.semary@gmail.com>
2015-12-16 14:09:52 +0300

    Initial Commit

---
+++ My Favorite ActorsTests/My_Favorite_ActorsTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_Favorite_ActorsTests.swift
+//  My Favorite ActorsTests
+//
+//  Created by Abdelrahman Mohamed on 12/16/15.
+//  Copyright © 2015 Abdelrahman Mohamed. All rights reserved.
+//
+
+import XCTest
+@testable import My_Favorite_Actors
+
+class My_Favorite_ActorsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
